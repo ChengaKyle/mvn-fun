24d6f40011dba5133f79a4ecbaab363121d22bf1
Tobias <s84458@bht-berlin.de>
2025-04-24 22:02:54 +0200

    add Factorizer.java

---
+++ src/main/java/com/mycompany/app/Factorizer.java
@@ -0,0 +1,36 @@
+package com.mycompany.app;
+
+import java.util.*;
+
+public class Factorizer {
+    private static Factorizer instance;
+
+    private Factorizer() {
+        // private constructor
+    }
+
+    public static Factorizer getInstance() {
+        if (instance == null) {
+            instance = new Factorizer();
+        }
+        return instance;
+    }
+
+    public List<Integer> factorize(int n) {
+        List<Integer> factors = new ArrayList<>();
+        if (n < 2) return factors;
+
+        for (int i = 2; i <= n / i; i++) {
+            while (n % i == 0) {
+                factors.add(i);
+                n /= i;
+            }
+        }
+
+        if (n > 1) {
+            factors.add(n);
+        }
+
+        return factors;
+    }
+}
